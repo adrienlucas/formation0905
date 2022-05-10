df09cd0e67c6c72cffa495ff542a0f102840d8a1
Adrien LUCAS <adrien.lucas@sensiolabs.com>
2022-05-10 11:03:50 +0200

    Install bootstrap & style-up the pages

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,7 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
+import 'bootstrap/scss/bootstrap.scss'
+
 // start the Stimulus application
 // import './bootstrap';
